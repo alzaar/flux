bc1e54bd80a99d7e875b452df261ec188b812b28
ali zahir <alizahir@live.com>
2018-11-19 18:43:07 -0500

    setting up files

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>FLULUX</h1>
+      </div>
+    );
+  }
+}
